fd44a4dc3fb5cf96811abf920a21960b0c309c24
Adam Minchella <adamminchella@msn.com>
2022-11-03 17:31:14 +0000

    Add CSS animations to hamburger icon on click

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ const hamburgerIcon = document.querySelector(".hamburger");
 const navList = document.querySelector("ul");
 
 hamburgerIcon.addEventListener("click", () => {
+  hamburgerIcon.classList.toggle("active");
   navList.classList.toggle("active");
 });
